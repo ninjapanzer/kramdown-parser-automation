f3afaa1b1f2503384c55eb1ade99b699f2b1405b
Paul Scarrone <pscarrone@Pauls-MacBook-Pro.local>
2022-03-22 13:08:58 -0400

    Allow for mixing in loaded extenions into another kramdown parser

--- lib/kramdown/parser/automation.rb
+++ lib/kramdown/parser/automation.rb
@@ -7,7 +7,7 @@ require_relative 'automation_version'
 module Kramdown
   module Parser
 
-    # This class provides a parser implementation for the GFM dialect of Markdown.
+    # This class provides a parser implementation for the Automation dialect of Markdown.
     class Automation < Kramdown::Parser::Kramdown
 
       VERSION = Parser::AUTOMATION_VERSION
@@ -40,17 +40,22 @@ module Kramdown
       end
 
       def self.define_parser(name, start_re, span_start = nil, meth_name = "parse_#{name}")
-        define_method("parse_#{name}".to_sym) do |src, tree|
-          send("parse_#{parser}".to_sym, src, tree)
-        end
         REGISTRY.push(name)
         super
+      end
+
+      def self.mixin_registry(target)
+        REGISTRY.each do |ext|
+          target.define_singleton_method("parse_#{ext}".to_sym) do
+            Kramdown::Parser::Automation.send("parse_#{ext}".to_sym, target, target.src, target.tree)
+          end
+        end
       end
 
       def initialize(source, options)
         super
         @block_parsers.unshift(:codeblock_automation)
-        self.class.load_extension(:automation)
+        self.class.load_extension(:codeblock_automation)
       end
 
     end
--- lib/kramdown/parser/automation/automation_parser.rb
+++ lib/kramdown/parser/automation/automation_parser.rb
@@ -1,8 +1,8 @@
 module Kramdown
   module Parser
     class Automation
-      
-      FENCED_CODEBLOCK_AUTOMATION_START_PATTERN = /.(\w*)(?:.[{](\s*[a-zA-Z0-9]+=\".+\")+[}])?/m
+
+      FENCED_CODEBLOCK_AUTOMATION_START_PATTERN = /\s*(\w*)(?:.[{](\s*[a-zA-Z0-9]+=\".+\")+[}])?/m
       FENCED_CODEBLOCK_START = ::Kramdown::Parser::Kramdown::FENCED_CODEBLOCK_START
       FENCED_CODEBLOCK_START_PATTERN = -> {
         start_pattern = FENCED_CODEBLOCK_START
@@ -14,7 +14,7 @@ module Kramdown
         )
       }
 
-      FENCED_CODEBLOCK_MATCH = /^([ ]{0,3}[~`]{3,})\W*(\w*)(?:\W*[{](\s*[a-zA-Z0-9-]+=\".+\")+[}])\W*\n([\S\W\n]*)\n\1/m
+      FENCED_CODEBLOCK_MATCH = /^([ ]{0,3}[~`]{3,})\s*(\w*)(?:\W*[{](\s*[a-zA-Z0-9-]+=\".+\")+[}])\W*\n([\S\W\n]*)\n\1/m
       FENCED_CODEBLOCK_MATCH_PATTERN = -> {
         match_pattern = FENCED_CODEBLOCK_MATCH
         match_pattern = OPTIONS[:codeblock_automation][:match] if OPTIONS[:codeblock_automation]&.has_key?(:match)
@@ -28,7 +28,7 @@ module Kramdown
         if src.check(FENCED_CODEBLOCK_MATCH_PATTERN.())
           start_line_number = src.current_line_number
           src.pos += src.matched_size
-          el = base.new_block_el(:codeblock, src[4], nil, location: start_line_number, fenced: true)
+          el = base.send(:new_block_el, :codeblock, src[4], nil, location: start_line_number, fenced: true)
           lang = src[2].to_s.strip
           unless lang.empty?
             el.options[:lang] = lang
@@ -39,7 +39,6 @@ module Kramdown
           unless automation.empty?
             el.options[:automation] = automation
           end
-
           puts "Automation Config: #{automation}"
 
           tree.children << el
@@ -49,7 +48,6 @@ module Kramdown
         end
       end
       define_parser(:codeblock_automation, FENCED_CODEBLOCK_START_PATTERN.(), nil)
-
     end
   end
 end
